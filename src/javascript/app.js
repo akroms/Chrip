1c59f5ca1e9ff67898c63a7cd5742bb90ac1ba85
akroms <Akromiphone7.90@gmail.com>
2021-09-18 20:18:35 +0500

    JavaScript Togle

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,31 +2,26 @@
 
 let toggle = document.querySelector(".navbar-toggle"); 
 let navbarStyle = `
-    /* Toggle */
-    
-    header .navbar .row {
-        padding: 2em 0;
-        justify-content: center;
-    }
-
-    header .navbar-list {
-        display: block;
-        text-align: center;
-    }
-    
-    header .navbar-item {
-        margin: 1em 0;
-    }
-
-    header .navbar-brand {
-        display: none;
-    }
-
-    header .navbar-toggle {
-        position: absolute;
-        top: 2em;
-        right: 0;
-    }
+/* Toggle */
+header .navbar .nav-row {
+  padding: 2em 0;
+  justify-content: center;
+}
+header .navbar-list {
+  display: block;
+  margin-left: auto;
+  text-align:center;
+}
+header .navbar-item {
+  margin: 1em 0;
+}
+header .navbar-brand {
+  display: none;
+}
+header .navbar-toggle {
+  align-self: flex-start;
+  margin-left: auto;
+}
 `; 
 
 let createStyle = document.createElement("style"); 
